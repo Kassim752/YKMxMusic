bc38d91b3d579e2117340f6d6d65778ea4c4953a
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-23 23:10:52 +0530

    🌸🔥

--- LegendX/utils/decorators/language.py
+++ LegendX/utils/decorators/language.py
@@ -1,6 +1,6 @@
 from strings import get_string
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import (get_lang, is_commanddelete_on,
+from LegendX.misc import SUDOERS
+from LegendX.utils.database import (get_lang, is_commanddelete_on,
                                        is_maintenance)
 
 
