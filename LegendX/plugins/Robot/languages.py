9fa8eddf8afb7e412b09f96f1a7874b827660c79
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-24 00:53:22 +0530

    🌸🔥

--- LegendX/plugins/Robot/languages.py
+++ LegendX/plugins/Robot/languages.py
@@ -4,9 +4,9 @@ from pyrogram.types import InlineKeyboardButton, Message
 
 from config import BANNED_USERS
 from strings import get_command, get_string
-from AnonX import app
-from AnonX.utils.database import get_lang, set_lang
-from AnonX.utils.decorators import (ActualAdminCB, language,
+from LegendX import app
+from LegendX.utils.database import get_lang, set_lang
+from LegendX.utils.decorators import (ActualAdminCB, language,
                                          languageCB)
 
 # Languages Available
