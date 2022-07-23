9890ea23b3adc33d94a155e6a8e890e50c251daa
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-23 22:49:48 +0530

    🌸🔥

--- LegendX/utils/channelplay.py
+++ LegendX/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonX import app
-from AnonX.utils.database import get_cmode
+from LegendX import app
+from LegendX.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
