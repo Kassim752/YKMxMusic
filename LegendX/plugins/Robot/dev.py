3fb17fb2fafb7d277403ddbf388276848fc47502
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-24 00:40:44 +0530

    🌸🔥

--- LegendX/plugins/Robot/dev.py
+++ LegendX/plugins/Robot/dev.py
@@ -11,8 +11,8 @@ from pyrogram import filters
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
-from AnonX import app
-from AnonX.misc import SUDOERS
+from LegendX import app
+from LegendX.misc import SUDOERS
 
 
 async def aexec(code, client, message):
