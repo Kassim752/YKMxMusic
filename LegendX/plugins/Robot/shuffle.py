d541bccafad1da7fa334bb34bc744a8d076f8101
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-23 23:45:12 +0530

    🌸🔥

--- LegendX/plugins/Robot/shuffle.py
+++ LegendX/plugins/Robot/shuffle.py
@@ -5,9 +5,9 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from AnonX import app
-from AnonX.misc import db
-from AnonX.utils.decorators import AdminRightsCheck
+from LegendX import app
+from LegendX.misc import db
+from LegendX.utils.decorators import AdminRightsCheck
 
 # Commands
 SHUFFLE_COMMAND = get_command("SHUFFLE_COMMAND")
