f4ecf9fa4057a17da110fc1a82a84680e7026d6c
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-23 23:09:36 +0530

    🌸🔥

--- LegendX/utils/decorators/play.py
+++ LegendX/utils/decorators/play.py
@@ -2,15 +2,15 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import PLAYLIST_IMG_URL, PRIVATE_BOT_MODE, adminlist
 from strings import get_string
-from AnonX import YouTube, app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database import (get_cmode, get_lang,
+from LegendX import YouTube, app
+from LegendX.misc import SUDOERS
+from LegendX.utils.database import (get_cmode, get_lang,
                                        get_playmode, get_playtype,
                                        is_active_chat,
                                        is_commanddelete_on,
                                        is_served_private_chat)
-from AnonX.utils.database.memorydatabase import is_maintenance
-from AnonX.utils.inline.playlist import botplaylist_markup
+from LegendX.utils.database.memorydatabase import is_maintenance
+from LegendX.utils.inline.playlist import botplaylist_markup
 
 
 def PlayWrapper(command):
