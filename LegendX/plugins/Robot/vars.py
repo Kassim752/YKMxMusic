698611fedf30baad748571b2ec15ed2266590d12
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-23 23:23:12 +0530

    🌸🔥

--- LegendX/plugins/Robot/vars.py
+++ LegendX/plugins/Robot/vars.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from AnonX import app
-from AnonX.misc import SUDOERS
-from AnonX.utils.database.memorydatabase import get_video_limit
-from AnonX.utils.formatters import convert_bytes
+from LegendX import app
+from LegendX.misc import SUDOERS
+from LegendX.utils.database.memorydatabase import get_video_limit
+from LegendX.utils.formatters import convert_bytes
 
 VARS_COMMAND = get_command("VARS_COMMAND")
 
