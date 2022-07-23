8c0eb7913fd3030f2c022a58dc59a60c8d142e0e
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-24 00:22:15 +0530

    🌸🔥

--- LegendX/plugins/Robot/play.py
+++ LegendX/plugins/Robot/play.py
@@ -10,21 +10,21 @@ from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from config import BANNED_USERS, lyrical
 from strings import get_command
-from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram,
+from LegendX import (Apple, Resso, SoundCloud, Spotify, Telegram,
                         YouTube, app)
-from AnonX.core.call import Anon
-from AnonX.utils import seconds_to_min, time_to_seconds
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.database import is_video_allowed
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.decorators.play import PlayWrapper
-from AnonX.utils.formatters import formats
-from AnonX.utils.inline.play import (livestream_markup,
+from LegendX.core.call import Legend
+from LegendX.utils import seconds_to_min, time_to_seconds
+from LegendX.utils.channelplay import get_channeplayCB
+from LegendX.utils.database import is_video_allowed
+from LegendX.utils.decorators.language import languageCB
+from LegendX.utils.decorators.play import PlayWrapper
+from LegendX.utils.formatters import formats
+from LegendX.utils.inline.play import (livestream_markup,
                                           playlist_markup,
                                           slider_markup, track_markup)
-from AnonX.utils.inline.playlist import botplaylist_markup
-from AnonX.utils.logger import play_logs
-from AnonX.utils.stream.stream import stream
+from LegendX.utils.inline.playlist import botplaylist_markup
+from LegendX.utils.logger import play_logs
+from LegendX.utils.stream.stream import stream
 
 # Command
 PLAY_COMMAND = get_command("PLAY_COMMAND")
