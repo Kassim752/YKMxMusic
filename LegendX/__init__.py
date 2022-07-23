03730e61a9b7e61fc7f968e0e5eeb7d435658996
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-23 17:11:15 +0530

    🌸🔥

--- LegendX/__init__.py
+++ LegendX/__init__.py
@@ -1,8 +1,8 @@
-from AnonX.core.bot import AnonXBot
-from AnonX.core.dir import dirr
-from AnonX.core.git import git
-from AnonX.core.userbot import Userbot
-from AnonX.misc import dbb, heroku, sudo
+from LegendX.core.bot import LegendXBot
+from LegendX.core.dir import dirr
+from LegendX.core.git import git
+from LegendX.core.userbot import Userbot
+from LegendX.misc import dbb, heroku, sudo
 from aiohttp import ClientSession
 
 from .logging import LOGGER
