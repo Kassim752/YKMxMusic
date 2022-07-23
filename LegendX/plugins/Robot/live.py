c0bcb38a4c3221458ccafb5fd8d51a6b7488b1ee
TeamLegend77 <109813393+TeamLegend77@users.noreply.github.com>
2022-07-24 00:31:08 +0530

    🌸🔥

--- LegendX/plugins/Robot/live.py
+++ LegendX/plugins/Robot/live.py
@@ -1,10 +1,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from AnonX import YouTube, app
-from AnonX.utils.channelplay import get_channeplayCB
-from AnonX.utils.decorators.language import languageCB
-from AnonX.utils.stream.stream import stream
+from LegendX import YouTube, app
+from LegendX.utils.channelplay import get_channeplayCB
+from LegendX.utils.decorators.language import languageCB
+from LegendX.utils.stream.stream import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
